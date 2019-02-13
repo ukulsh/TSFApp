17799333c7f574eb5e1f07bf8402fe3165225f20
utkarsh <kulshrestha.utkarsh@outlook.com>
2019-02-13 17:18:39 +0530

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/TsfApp/activities/ProfileActivity.java
@@ -0,0 +1,74 @@
+package net.simplifiedcoding.TsfApp.activities;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import net.simplifiedcoding.TsfApp.R;
+import net.simplifiedcoding.TsfApp.fragments.HomeFragment;
+import net.simplifiedcoding.TsfApp.fragments.SettingsFragment;
+import net.simplifiedcoding.TsfApp.fragments.UsersFragment;
+import net.simplifiedcoding.TsfApp.storage.SharedPrefManager;
+
+public class ProfileActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+        displayFragment(new HomeFragment());
+    }
+
+    private void displayFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.relativeLayout, fragment)
+                .commit();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (!SharedPrefManager.getInstance(this).isLoggedIn()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch(item.getItemId()){
+            case R.id.menu_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.menu_users:
+                fragment = new UsersFragment();
+                break;
+            case R.id.menu_settings:
+                fragment = new SettingsFragment();
+                break;
+        }
+
+        if(fragment != null){
+            displayFragment(fragment);
+        }
+
+        return false;
+    }
+}
